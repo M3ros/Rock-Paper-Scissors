e8dda696c910382075d88bc80ab63d4e88e96c01
Christian Stewart <86859886+M3ros@users.noreply.github.com>
2023-06-21 23:56:24 -0500

    Dom Updates
    Working on making a GUI for the game based off the DOM

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const options = ["ROCK", "PAPER", "SCISSORS"];
-// let playerSelection = getPlayerChoice();
+let playerScore = 0;
+let cpuScore = 0;
 
 function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
@@ -17,21 +18,26 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection == "SCISSORS" && computerSelection == "ROCK")
   ) {
     console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+    cpuScore++;
+    document.getElementById("cScore").innerHTML = cpuScore;
+    console.log("cpu " + cpuScore);
   } else if (playerSelection == computerSelection) {
     console.log(
       "It's a draw! " + playerSelection + " draws " + computerSelection
     );
   } else {
     console.log("You Win! " + playerSelection + " beats " + computerSelection);
+    playerScore++;
+    document.getElementById("pScore").innerHTML = playerScore;
+    console.log("player " + playerScore);
   }
 }
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    let computerSelection = getComputerChoice();
-    let playerSelection = getPlayerChoice();
-    playRound(playerSelection, computerSelection);
-  }
+  let computerSelection = getComputerChoice();
+  let playerSelection = getPlayerChoice();
+  playRound(playerSelection, computerSelection);
 }
 
-game();
+// game();
+// for (let i = 0; i < 5; i++)
